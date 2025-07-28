8fa9543cdd92a3c25c633c6cd48d2d65b2b1d822
WIN-QCLQ02RU5LA\Svyat <3yxpylichka228@gmail.com>
2025-07-28 12:03:04 +0300

    Запрограммировал первые два юзкейса
    (cherry picked from commit 64eb3b46f69f120c48a869cad66dce768fb4119d)

---
+++ MusicStore/Infrastructure/TestRepository.cs
@@ -0,0 +1,12 @@
+﻿using Application;
+
+namespace Infrastructure
+{
+    public class TestRepository : IRepository
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MusicStore/MusicStore.Application/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using MusicStore.Application.Interfaces;
+using MusicStore.Domain.Entities.Users;
+
+namespace MusicStore.Application.Queries
+{
+    public class GetUserByIdQuery : IQuery<User>
+    {
+        public Guid Id { get; set; }
+    }
+}
